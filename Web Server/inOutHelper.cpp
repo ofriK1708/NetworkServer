7b74e0e1737902f233be5b94e3731ed290bbffff
Ofri Kuperberg <kuperbergof@mta.ac.il>
2024-09-10 21:41:57 +0300

    improved the createResponse

--- Web Server/inOutHelper.cpp
+++ Web Server/inOutHelper.cpp
@@ -1,19 +1,30 @@
 #include "inOutHelper.h"
 #include <fstream>
-using std::ifstream;
-void createResponse(const string& status, const string& content_type, char* sendResponse, const string& body, int content_size, bool isHead, bool isOptions)
+#include <iostream>
+#include <ctime>
+
+
+#define HEAD 0
+#define GET 1
+#define OPTIONS 2
+#define POST 3
+#define PUT 4
+#define DELETE 5
+#define TRACE 6
+
+void createResponse(const string& status, const string& content_type, char* sendResponse, int method, int content_size, const string& body)
 {
 	time_t timer;
 	time(&timer);
 	string response = "HTTP/1.1 " + status + "\r\n";
 	response += "Date: " + string(ctime(&timer)) + "\r\n";
-	if (!body.empty() || isHead)
+	if (method == GET || method == HEAD)
 	{
 		response += "Content-Type: " + content_type + "\r\n";
 		response += "Server: WebServer/1.0\r\n";
 		response += "Content-Length: " + std::to_string(content_size) + "\r\n";
 	}
-	else if (isOptions)
+	else if (method == OPTIONS)
 	{
 		response += "Allow: GET, HEAD, OPTIONS, PUT, DELETE, TRACE\r\n";
 		response += "Server: WebServer/1.0\r\n";
@@ -27,36 +38,36 @@ void createResponse(const string& status, const string& content_type, char* send
 	sendResponse = (char*)response.c_str();
 }
 
-void GET_HEAD_request(string& path, char* response,bool isHead)
+void GET_HEAD_request(string& path, char* response, int method)
 {
 	string full_path = "./HTML_FILES"; // Base directory for the files
 	full_path += path;
     
-	ifstream file(full_path,std::ios::in);
+	std::ifstream file(full_path,std::ios::in);
 	// Read the file content
 	if (!file.is_open())
 	{
-		createResponse("404 Not Found", "text/html", response);
+		createResponse("404 Not Found", "text/html", response, GET); // here it doesnt matter if its head or get because file is not found or bad 
 		return;
 	}
 	if (file.bad())
 	{
-		createResponse("500 Internal Server Error", "text/html", response);
+		createResponse("500 Internal Server Error", "text/html", response,GET);
 		return;
 	}
 	file.seekg(0, file.end);
 	long int file_size = file.tellg();
 	file.seekg(0, file.beg);
-	if (!isHead) 
+	if (method == GET)
 	{
 		string body;
 		body.resize(file_size);
 		file.read(&body[0], file_size);
-		createResponse("200 OK", "text/html", response, body);
+		createResponse("200 OK", "text/html", response, GET, file_size, body);
 	}
 	else
 	{
-		createResponse("200 OK", "text/html", response, "", file_size, true);
+		createResponse("200 OK", "text/html", response, HEAD, file_size);
 	}
 	file.close();
 }
--- Web Server/inOutHelper.h
+++ Web Server/inOutHelper.h
@@ -1,9 +1,8 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <string>
-#include <ctime>
 using std::string;
 
-void createResponse(const string& status, const string& content_type, char* sendResponse, const string& body = "", int content_size = 0, bool isHead = false, bool isOptions = false);
-void GET_HEAD_request(string& path, char* response, bool isHead = false);
+void createResponse(const string& status, const string& content_type, char* sendResponse,int method, int content_size = 0, const string& body = "");
+void GET_HEAD_request(string& path, char* response, int method);
